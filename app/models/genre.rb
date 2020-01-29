23c3936036b55c68a6afe4add8e192f28a26fb64
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-29 15:36:26 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.genres.count.artist
+    self.artists.count
   end
 
   def all_artist_names
